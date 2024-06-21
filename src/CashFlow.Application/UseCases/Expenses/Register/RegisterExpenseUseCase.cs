b1407e4247a0a8975e45ac3619a519b68b84adec
fernandodamasceno <damasceno.developer21@gmail.com>
2024-06-21 09:12:58 -0300

    feat: add unit of work

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,6 +1,7 @@
 using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 using CashFlow.Domain.Entities;
+using CashFlow.Domain.Repositories;
 using CashFlow.Domain.Repositories.Expenses;
 using CashFlow.Exception.ExceptionBase;
 
@@ -9,10 +10,12 @@ namespace CashFlow.Application.UseCases.Expenses.Register;
 public class RegisterExpenseUseCase : IRegisterExpenseUseCase
 {
     private readonly IExpensesRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public RegisterExpenseUseCase(IExpensesRepository repository)
+    public RegisterExpenseUseCase(IExpensesRepository repository, IUnitOfWork unitOfWork)
     {
         _repository = repository;
+        _unitOfWork = unitOfWork;
     }
     public ResponseRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
     {
@@ -27,6 +30,7 @@ public class RegisterExpenseUseCase : IRegisterExpenseUseCase
         };
         
         _repository.Add(entity);
+        _unitOfWork.Commit();
 
         //Before dependency injection:
         // reference infra project and import using CashFlow.Infrastructure.DataAccess;
--- src/CashFlow.Domain/obj/Debug/net8.0/CashFlow.Domain.AssemblyInfo.cs
+++ src/CashFlow.Domain/obj/Debug/net8.0/CashFlow.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdb97a724f143e388eea2ace036cddee6cefc56f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a76d0dc1488da1c22a4a9bb6248b578de3e8e457")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
